8c6540dc34b1ad887f7f7d5fbe7ea77589ca86f1
jeeten routa <150784085+routjeeten10@users.noreply.github.com>
2024-06-03 16:37:07 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import SelectableGrid from "./components/selectable-grid";
+
+function App() {
+  return (
+    <div>
+      <h1>Selectable Grid</h1>
+      <SelectableGrid rows={10} cols={10} />
+      
+    </div>
+  );
+}
+
+export default App;
